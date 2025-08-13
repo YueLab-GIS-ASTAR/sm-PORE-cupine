102b44b0d2ceb869b4414bf29792cf4b1664b64e
QSQX-HAN <157657809+QSQX-HAN@users.noreply.github.com>
2025-08-13 09:36:16 +0800

    Update setup.py

--- sm-PORE-cupine-v0.1.0/setup.py
+++ sm-PORE-cupine-v0.1.0/setup.py
@@ -7,19 +7,19 @@ with open("requirements.txt") as f:
 
 # Note: the _program variable is set in __init__.py.
 # it determines the name of the package/final command line tool.
-from sigpore import __version__, _program
+from sm-PORE-cupine import __version__, _program
 
-setup(name="sigpore",
+setup(name="sm-PORE-cupine",
       version=__version__,
       packages=["sm-PORE-cupine"],
       description="Pipeline to perform signal alignment of nanopore signal traces to a signal reference",
       entry_points="""
       [console_scripts]
-      {program} = sigpore.execute_workflows:main
+      {program} = sm-PORE-cupine.execute_workflows:main
       """.format(program = _program),
       install_requires=required,
       include_package_data=True,
       package_data={
-          "sigpore": ['Snakefile','config/cluster_pluto.json', 'config/config_env_pluto.yaml']
+          "sm-PORE-cupine": ['Snakefile','config/cluster_pluto.json', 'config/config_env_pluto.yaml']
       },
       keywords=[])
